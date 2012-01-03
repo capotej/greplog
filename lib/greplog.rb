d242856a3dbfc85f516dc6a75c809618490fe1e8
Julio Capote <jcapote@gmail.com>
2012-01-02 16:41:03 -0800

    boots

---
+++ lib/greplog.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'eventmachine'
+require 'evma_httpserver'
+require 'lib/server'
+
+module Greplog
+  # Your code goes here...
+end
---
+++ lib/greplog/version.rb
@@ -0,0 +1,3 @@
+module Greplog
+  VERSION = "0.0.1"
+end
